51c829a9acb02adfd8cae654703ae7afcf83111c
Elheid <Elfieleintsvard@mail.ru>
2024-09-14 20:48:57 +0500

    refactorDir

---
+++ src/components/ServicesPage/ServiceCards/CardParts/ClearCardParts.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+
+import arrowSVG from "../../../../img/arrow.svg";
+
+/// Clear card components
+interface ClearCardIconComponentProps {
+    iconSrc: string;
+}
+const loadSVG = async (svgUrl: string) => {
+    try {
+        const response = await fetch(svgUrl);
+        const svgText = await response.text();
+
+        // Parse the SVG using svgo
+        const parser = new DOMParser();
+        const svgDoc = parser.parseFromString(svgText, 'image/svg+xml');
+
+
+        const fillElements = svgDoc.querySelectorAll('[fill]');
+        fillElements.forEach((el) => {
+            el.setAttribute('fill', '#ffffff');
+        });
+
+        svgDoc.documentElement.style.fill = 'white';
+
+        // Create a new URL for the modified SVG
+        const res = URL.createObjectURL(new Blob([svgDoc.documentElement.outerHTML], { type: 'image/svg+xml' }));;
+        return res;
+
+    } catch (error) {
+        console.error('Error loading or modifying SVG:', error);
+    }
+}
+
+const ClearCardIconComponent = ({ iconSrc }: ClearCardIconComponentProps) => {
+    //const getBlobIconUrl = loadSVG(iconSrc);
+    const [icon, setIcon] = useState(iconSrc);
+
+    useEffect(() => {
+        const fetchIcon = async () => {
+            const iconUrl = await loadSVG(iconSrc);
+            if (iconUrl) {
+                setIcon(iconUrl);
+            }
+        }
+        fetchIcon();
+    }, [])
+    return (
+        <div className="icon-container">
+            <img src={icon} alt="Icon" />
+        </div>
+    );
+}
+
+interface ClearCardHeaderProps {
+    childrenCount: number;
+    title: string;
+}
+
+
+const ClearCardHeader = ({ title, childrenCount }: ClearCardHeaderProps) => {
+    return (
+        <div className="card-header">
+            <h3 className="card-title card-description">{title}</h3>
+            <div className="card-footer">
+                <p className="count-services">услуг: {childrenCount}</p>
+                <img className="arrow-img" src={arrowSVG} alt="Arrow" />
+            </div>
+        </div>
+    );
+}
+export {ClearCardHeader, ClearCardIconComponent}
---
+++ src/components/ServicesPage/ServiceCards/CardParts/GesturalParts.tsx
@@ -0,0 +1,84 @@
+import arrowLargeSVG from "../../../../img/arrowLarge.svg";
+///Gestural card components 
+
+// Интерфейс для описания ошибки
+interface VideoError {
+    message: string;
+}
+
+// Настраиваемый хук для управления видеоплеером
+
+//Не помагает
+/*
+const useVideoHandleError = (videoUrl: string)=>{
+    const [error, setError] = useState<VideoError | null>(null); // Состояние ошибки
+    const videoRef = useRef<HTMLVideoElement>(null); // Ссылка на элемент видео
+    let retryCount = 0; // Счетчик попыток
+    const maxRetries = 3; // Максимальное количество попыток
+
+    // Эффект для обработки ошибки загрузки видео
+    useEffect(() => {
+        const handleError = (event: ErrorEvent) => {
+            const error = event.error as VideoError;
+            console.log(error)
+            if (error.message.includes('net::ERR_CACHE_OPERATION_NOT_SUPPORTED') && retryCount < maxRetries) {
+                retryCount++;
+                setTimeout(() => {
+                    videoRef.current!.src = videoUrl;
+                    videoRef.current!.load();
+                }, 1000 * retryCount); // Задержка между попытками
+            } else {
+                setError(error);
+            }
+        };
+
+        const videoElement = videoRef.current;
+        if (videoElement) {
+            videoElement.addEventListener('error', handleError);
+        }
+
+        // Очистка обработчика при размонтировании компонента
+        return () => {
+            if (videoElement) {
+                videoElement.removeEventListener('error', handleError);
+            }
+        };
+    }, [videoUrl]);
+
+    return { error, videoRef };
+}*/
+
+interface GesturalVideoComponentProps {
+    gifSrc: string;
+}
+
+const GesturalVideoComponent = ({ gifSrc }: GesturalVideoComponentProps) => {
+    //const { error, videoRef } = useVideoHandleError(gifSrc); // Замените URL на ваш
+    return (
+        <div className="video-overlay">
+            {/*error && <p>Ошибка загрузки видео: {error.message}</p>*/} 
+            <video className="gif"
+                src={gifSrc}
+                playsInline={true}
+                loop={true}
+                autoPlay={true}
+                muted={true}>
+            </video>
+        </div>
+    );
+}
+
+interface GesturalCardSubstrateComponentProps {
+    title: string;
+}
+
+const GesturalCardSubstrateComponent = ({ title }: GesturalCardSubstrateComponentProps) => {
+    return (
+        <div className="substrate">
+            <h3 className="card-title card-description">{title}</h3>
+            <img src={arrowLargeSVG} />
+        </div>
+    );
+}
+
+export {GesturalCardSubstrateComponent, GesturalVideoComponent}
